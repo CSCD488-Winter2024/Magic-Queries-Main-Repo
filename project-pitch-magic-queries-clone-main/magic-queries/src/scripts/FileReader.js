ce70b19d83cb4b607a7ad95c8826532c6b854a69
Hunter Allen <103107532+hallen10@users.noreply.github.com>
2024-04-25 10:04:04 -0700

    Update FileReader.js
    Added new push function to the database. Added new search function.

--- project-pitch-magic-queries-clone-main/magic-queries/src/scripts/FileReader.js
+++ project-pitch-magic-queries-clone-main/magic-queries/src/scripts/FileReader.js
@@ -1,28 +1,25 @@
 const apiUrl = 'https://api.scryfall.com'; // Base URL of the Scryfall API
 // import firebase from 'firebase/app';
 import { initializeApp } from 'firebase/app';
-import { getDatabase, onValue, ref, set } from 'firebase/database';
-
+import { getFirestore, doc, setDoc, getDoc, where, query, getDocs, collection, or, and, limit, startAfter } from "firebase/firestore";
 
 // Initialize Firebase
-const firebaseConfig = {
+const firebaseApp = initializeApp({
     apiKey: "AIzaSyCAVRElz4RmMuQD3-RQ5Ttd1w_h8MTStAc",
     authDomain: "magicproject-77014.firebaseapp.com",
     projectId: "magicproject-77014",
     storageBucket: "magicproject-77014.appspot.com",
     messagingSenderId: "1023897851903",  
     appId: "1:1023897851903:web:bbcd8bdf3e81c0ac82f9ca",
-    databaseURL: "https://magicproject-77014-default-rtdb.firebaseio.com/"
-};
+})
 
 class cardInfo {
-    constructor(set, name, color, id, rarity, artist, quantity, picURL, type, price){
+    constructor(set, name, color, id, rarity, quantity, picURL, type, price){
         this.set = set;
         this.name = name;
         this.color = color;
         this.id = id;
         this.rarity = rarity;
-        this.artist = artist;
         this.quantity = quantity;
         this.picURL = picURL;
         this.type = type;
@@ -31,26 +28,28 @@ class cardInfo {
 }
 
 function writeDataToFirebase(cardList) {
-    const app = initializeApp(firebaseConfig);
-    const db = getDatabase(app);
+    const firestore = getFirestore();
+    // for each card in cardList, write to firebase
     cardList.forEach(card => {
-        var cardListRef = ref(db, 'MagicCards/' + card.set + '/' + card.name);
-        set(cardListRef, {
-            set: card.set,
-            name: card.name,
-            color: card.color,
-            id: card.id,
-            rarity: card.rarity,
-            artist: card.artist,
-            quantity: card.quantity,
-            picURL: card.picURL,
-            type: card.type,
-            price: card.price
-        });
-        // exit loop after 10 cards
-        if (cardList.indexOf(card) === 10) {
-            return;
-        }
+        // create a name array to store parts of the lowercase name
+        const nameArray = card.name.toLowerCase().split(' ');
+
+        const docRef = doc(firestore, 'MagicCards/' + card.name + '^' + card.set);
+        const docData = {
+            // if value is undefined, set it to "N/A"
+            name: card.name === undefined ? "N/A" : card.name,
+            set: card.set === undefined ? "N/A" : card.set,
+            color: card.color === undefined ? "N/A" : card.color,
+            id: card.id === undefined ? "N/A" : card.id,
+            rarity: card.rarity === undefined ? "N/A" : card.rarity,
+            // make quantity a number
+            quantity: card.quantity === undefined ? 0 : parseInt(card.quantity),
+            picURL: card.picURL === undefined ? "N/A" : card.picURL,
+            type: card.type === undefined ? "N/A" : card.type,
+            price: card.price === undefined ? "N/A" : card.price,
+            nameArray: nameArray
+        };
+        setDoc(docRef, docData);
     });
 }    
 
@@ -85,7 +84,6 @@ function readAllMagicCards(excelCardList) {
             const color = "";
             const id = "";
             const rarity = "";
-            const artist = "";
             const quantity = "";
             // try to get image_uris.png, if not, leave it empty
             try {
@@ -95,17 +93,17 @@ function readAllMagicCards(excelCardList) {
             }
             const type = card.type_line;
             const price = card.prices.usd;
-            cardList.push(new cardInfo(set, name, color, id, rarity, artist, quantity, picURL, type, price));
+            cardList.push(new cardInfo(set, name, color, id, rarity, quantity, picURL, type, price));
         });
         console.log("Read allMagicCards.json successfully.");
         // loop through excelCardList and find the corresponding card in cardList
         const combinedCard = [];
         excelCardList.forEach(excelCard => {
             cardList.forEach(card => {
-                if (excelCard.name === card.name) {
+                if (excelCard.name === card.name && excelCard.set === card.set) {
                     // combine excel and scryfall data
                     ///////////////////////////////////////////////////////////////////////////////////////////////////////
-                    combinedCard.push(new cardInfo(excelCard.set, excelCard.name, excelCard.color, excelCard.id, excelCard.rarity, excelCard.artist, excelCard.quantity, card.picURL, card.type, card.price));
+                    combinedCard.push(new cardInfo(excelCard.set, excelCard.name, excelCard.color, excelCard.id, excelCard.rarity, excelCard.quantity, card.picURL, card.type, card.price));
                     console.log(combinedCard);
                 }
             });
@@ -137,14 +135,13 @@ function readExcelFile(fileURL) {
         const color = card['Color'];
         const id = card['#'];
         const rarity = card['Rarity'];
-        const artist = card['Artist'];
         const quantity = card['Quantity'];
         const picURL = card['PicURL'];
         const type = card['Type'];
         const price = card['Price'];
 
         // create card object
-        const cardObj = new cardInfo(set, name, color, id, rarity, artist, quantity, picURL, type, price);
+        const cardObj = new cardInfo(set, name, color, id, rarity, quantity, picURL, type, price);
         cardList.push(cardObj);
         });
         console.log("Read excel file successfully.");
@@ -155,16 +152,36 @@ function readExcelFile(fileURL) {
     });
 }
 
-function getDatabaseCards() {
-    const app = initializeApp(firebaseConfig);
-    const db = getDatabase(app);
-    const dbRef = ref(db, 'MagicCards/');
-    onValue(dbRef, (snapshot) => {
-        const data = snapshot.val();
-        console.log(data);
+var lastDoc = null;
+var prevQuery = null;
+var prevPage = null;
+
+function searchDatabase(name, rarity, color, type, set, page){
+    const firestore = getFirestore();
+    name = name.toLowerCase();
+    const capitalized = name.charAt(0).toUpperCase() + name.slice(1);
+
+    const namePart = name === '' ? '' : or(and(where('name', '>=', capitalized), where('name', '<=', capitalized + '\uf8ff')), where('nameArray', 'array-contains-any', name.split(' ')));
+    const rarityPart = rarity === '' ? '' : where('rarity', '==', rarity);
+    const colorPart = color === '' ? '' : where('color', '==', color);
+    const typePart = type === '' ? '' : where('type', '==', type);
+    const setPart = set === '' ? '' : where('set', '==', set);
+    const pagePart = page === '' ? '' : startAfter(lastDoc);
+
+    const queryParts = [namePart, rarityPart, colorPart, typePart, setPart, pagePart].filter(part => part !== '');
+
+    var myQuery = query(collection(firestore, 'MagicCards'), and(...queryParts), limit(10));
+
+    getDocs(myQuery).then(snapshot => {
+        snapshot.forEach(doc => {
+            console.log(doc.data());
+            lastDoc = doc;
+        });
+        console.log("Search complete.");
     });
 }
 
 //getDatabaseCards();
 
 //readExcelFile('Magicplaceholder.xlsx');
+//searchDatabase('of', '', '', '', '', '');
